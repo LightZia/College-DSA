6e0f064e44c4f7176bf97254aa6563c6b21e37a8
Ansari Farah <ziaisme@outlook.com>
2026-09-08 10:38:06 +0530

    Simple Calculator problem Solved

--- src/simCalc.java
+++ src/simCalc.java
@@ -1,2 +1,14 @@
-public class simCalc {
-}
+        import java.util.Scanner;
+        public class simCalc {
+            public static void main(String[] arg) {
+                Scanner inScan = new Scanner(System.in);
+                int X = inScan.nextInt();
+                int Y = inScan.nextInt();
+                int sub = X - Y;
+                int add = X + Y;
+                long mul = (long)X * Y;
+                System.out.println(X + " + " + Y + " = " + add);
+                System.out.println(X + " * " + Y + " = " + mul);
+                System.out.println(X + " - " + Y + " = " + sub);
+            }
+        }
