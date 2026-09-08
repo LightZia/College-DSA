3e97c3aef5376d6fb608627be00b7c9d5a6e6809
Ansari Farah <ziaisme@outlook.com>
2026-09-08 10:28:17 +0530

    newLine Problem Solved

--- src/newLine.java
+++ src/newLine.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
 public class newLine {
+    public static void main(String[] args) {
+        Scanner inScan = new Scanner(System.in);
+        int num = inScan.nextInt();
+        long bigNum = inScan.nextLong();
+        char abc = inScan.next().charAt(0);
+        float decNum = inScan.nextFloat();
+        double bigDecNum = inScan.nextDouble();
+        System.out.println(num);
+        System.out.println(bigNum);
+        System.out.println(abc);
+        System.out.println(decNum);
+        System.out.println(bigDecNum);
+    }
 }
