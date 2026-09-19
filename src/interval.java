72e1dac7098dad672e556174661459da9f1c89cc
Ansari Farah <ziaisme@outlook.com>
2026-09-19 16:26:03 +0530

    Intervals problem Completed

--- src/interval.java
+++ src/interval.java
@@ -1,43 +1,25 @@
 import java.util.Scanner;
 public class interval {
     public static void main(String[] args) {
-        Scanner inScan = new Scanner(System.in);
-        float input = inScan.nextFloat();
-        if (input > 0 && input <= 24) {
-            System.out.print("Interval (0,25]");
-        }
-        else if (input == 0) {
-            System.out.print("Interval [0,25)");
-        }
-        else if (input == 25) {
+
+        Scanner input = new Scanner(System.in);
+        double num = input.nextDouble();
+
+        if (num >= 0 && num <= 25) {
             System.out.print("Interval [0,25]");
         }
-        else if (input > 24 && input < 25) {
-            System.out.print("Interval [0,25]");
-        }
-
-        if (input > 25 && input < 50) {
+        if (num > 25 && num <= 50) {
             System.out.print("Interval (25,50]");
         }
-        else if (input == 50) {
-            System.out.print("Interval (25,50]");
-        }
-
-        if (input > 50 && input < 75) {
+        if (num > 50 && num <= 75) {
             System.out.print("Interval (50,75]");
         }
-        else if (input == 75) {
-            System.out.print("Interval (50,75]");
-        }
-
-        if (input > 75 && input < 100) {
+        if (num > 75 && num <= 100) {
             System.out.print("Interval (75,100]");
         }
-        else if (input == 100) {
-            System.out.print("Interval (75,100]");
-        }
-        else if (input < 0 || input > 100) {
+        if (num < 0 || num > 100) {
             System.out.print("Out of Intervals");
         }
+
     }
 }
