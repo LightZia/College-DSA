c487df15c5d525389ddaad638998aeb978da2233
Ansari Farah <ziaisme@outlook.com>
2026-09-23 15:16:17 +0530

    Sheet #2 -> PS-C -> 'Even, Odd, Positive and Negative'

--- src/eveOddPosNeg.java
+++ src/eveOddPosNeg.java
@@ -4,5 +4,27 @@ public class eveOddPosNeg {
         Scanner inScan = new Scanner(System.in);
         int num = inScan.nextInt();
 
+        int even = 0, odd = 0, pos = 0, neg = 0;
+        for (int i = 0; i<num; i++){
+            int a = inScan.nextInt();
+            if (a%2==0){
+                even++;
+            }
+            else {
+                odd++;
+            }
+            if (a > 0) {
+                pos++;
+            }
+            else if (a < 0) {
+                neg++;
+            }
+        }
+
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
+        System.out.println("Positive: " + pos);
+        System.out.println("Negative: " + neg);
+
     }
 }
